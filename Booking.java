748c49e1d401cfbefb8e7594434a5dd0a57791de
ariMrashid <91915317+ariMrashid@users.noreply.github.com>
2021-10-19 02:47:56 +0300

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,13 +1,15 @@
+packege classes;
 
 public class Booking{
-int customerID;
-int roomNumberID;
-int NumberOfNights;;
-int FoodID;
-int NumberofFoods;
-int AmountOfMoney;
-int PaidOfMoney;
-int Dept;
-date DateFrom;
-date DateTo;
+  
+      int customerID;
+      int roomNumberID;
+      int NumberOfNights;;
+      int FoodID;
+      int NumberofFoods;
+      int AmountOfMoney;
+      int PaidOfMoney;
+      int Dept;
+      date DateFrom;
+      date DateTo;
 }
